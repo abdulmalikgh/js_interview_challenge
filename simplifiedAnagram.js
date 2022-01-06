f3848695f0378c010557f8820170168b90243930
abdulmalikgh <musah.abdulmalikgh@gmail.com>
2022-01-06 14:02:06 +0000

    Simplified anagram function

--- simplifiedAnagram.js
+++ simplifiedAnagram.js
@@ -5,4 +5,5 @@ function simplifiedAnagram(stringOne, stringTwo){
     if(stringOne.length !== stringTwo.length) return false
     return sortString(stringOne) === sortString(stringTwo)
 }
+// trim str.replace(/[^\w]/g).join()
 console.info(simplifiedAnagram('friend','finder'))
